ed05feb446c2214c35aa75f9669fffb2a39a92ba
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-24 19:16:05 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, "全角文字を使用してください"} do 
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
+  validates :birthday, presence: true 
 end
