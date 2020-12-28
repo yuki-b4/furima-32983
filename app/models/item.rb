28f5d5add64fb13b068a5e7dd4c93fa25db2145f
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-28 17:44:27 +0900

    トップページからの遷移とバリデーションの再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
@@ -7,10 +8,11 @@ class Item < ApplicationRecord
   belongs_to :shipping_from
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
-  
+  validates :name, :description,:image, presence: true
+  with_options presence: true, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"} do
+    validates :price
+  end
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "は、￥300~¥9,999,999の範囲内で設定してください" }
   validates :category_id, numericality: {other_than: 1}
   validates :condition_id, numericality: {other_than: 1}
   validates :shipping_charge_id, numericality: {other_than: 1}
