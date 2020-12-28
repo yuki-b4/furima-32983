bfa6ea993660116d52af71697edde747d3bb91be
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-28 19:58:32 +0900

    アソシエーションの再設定とサンプル画像の取り込み

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
+  belongs_to :user
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
@@ -8,15 +9,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_from
   has_one_attached :image
 
-  validates :name, :description,:image, presence: true
-  with_options presence: true, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"} do
-    validates :price
-  end
+  validates :name, :description, :image, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
   validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "は、￥300~¥9,999,999の範囲内で設定してください" }
-  validates :category_id, numericality: {other_than: 1}
-  validates :condition_id, numericality: {other_than: 1}
-  validates :shipping_charge_id, numericality: {other_than: 1}
-  validates :shipping_day_id, numericality: {other_than: 1}
-  validates :shipping_from_id, numericality: {other_than: 1}
+  validates :category_id, :condition_id, :shipping_charge_id, :shipping_day_id, :shipping_from_id, numericality: {other_than: 1}
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
