b59e8f06924c0a4dd18ac1a63f29008d87c86d72
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-30 08:28:07 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,9 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    end
   end
 
   private
