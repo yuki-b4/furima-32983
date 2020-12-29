ecd2a004dc96c4e21d5d743c731c69ba32745c29
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-29 18:09:26 +0900

    コントローラー・ルーティングの設定と画面遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
