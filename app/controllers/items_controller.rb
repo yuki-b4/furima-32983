b2a7860fff40ad5bc04bd2a396a7938d4e91408e
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-30 10:56:09 +0900

    可読性の向上と、出品者以外のログインユーザに対するページ遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
