aab9a3aa081236da206a2fb3177498261fadf1e6
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-04 12:12:04 +0900

    ユーザーのログイン・ログアウト状態にあわせたページ遷移

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,17 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: :index
+
+
   def index
     @item = Item.find(params[:item_id])
     @purchase = Purchase.new 
   end
 
-  
+  private
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id 
+      redirect_to :index
+    end
 end
