1de64426f4703a1f3e8f087559a5da08e4c4fd83
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-28 20:04:45 +0900

    プレビュー機能の実装

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,27 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/)) {
+  document.addEventListener("DOMContentLoaded", () => {
+    const ImageList = document.getElementById("image-list");
+
+    const createHTML = (blob) => {
+      const imageElement = document.createElement("div")
+        const blobImage = document.createElement("img")
+        blobImage.setAttribute("src", blob)
+    
+        imageElement.appendChild(blobImage)
+        ImageList.appendChild(imageElement)
+    }
+    
+      document.getElementById("item-image").addEventListener("change", (e) => {
+
+        const imageContent = document.querySelector("img");
+        if (imageContent){
+          imageContent.remove();
+        }
+        const file = e.target.files[0];
+        const blob = window.URL.createObjectURL(file);
+    
+        createHTML(blob)
+      })
+    })
+}
+
