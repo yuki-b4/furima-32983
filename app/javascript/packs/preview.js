6328090144987b8b78332187ceb7702ea3d5ff0e
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-28 21:29:59 +0900

    editビューへ画像を挿入するdivタグを追加

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,27 +1,26 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/)) {
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener("DOMContentLoaded", () => {
     const ImageList = document.getElementById("image-list");
 
-    const createHTML = (blob) => {
-      const imageElement = document.createElement("div")
-        const blobImage = document.createElement("img")
-        blobImage.setAttribute("src", blob)
+    const createImageHTML = (blob) => {
+        const imageElement = document.createElement("div");
+        const blobImage = document.createElement("img");
+        blobImage.setAttribute("src", blob);
     
-        imageElement.appendChild(blobImage)
-        ImageList.appendChild(imageElement)
-    }
+        imageElement.appendChild(blobImage);
+        ImageList.appendChild(imageElement);
+    };
     
       document.getElementById("item-image").addEventListener("change", (e) => {
 
         const imageContent = document.querySelector("img");
-        if (imageContent){
+        if (imageContent) {
           imageContent.remove();
         }
         const file = e.target.files[0];
         const blob = window.URL.createObjectURL(file);
     
-        createHTML(blob)
-      })
-    })
+        createImageHTML(blob);
+      });
+    });
 }
-
